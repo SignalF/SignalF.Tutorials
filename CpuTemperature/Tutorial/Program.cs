52e6d1f2be0a219ee10e340c57ec38a4e405198b
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-03-18 20:33:59 +0100

    Added initial code.

---
+++ CpuTemperature/Tutorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Versioning;
+using SignalF.Controller;
+using SignalF.Controller.Configuration;
+using SignalF.Extensions.Configuration;
+using SignalF.Extensions.Controller;
+
+namespace Tutorial;
+
+[SupportedOSPlatform("linux")]
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var options = CliOptionParser.ParseCliArguments(new ApplicationArgumentCollection());
+        if (options.ShowHelp)
+        {
+            return;
+        }
+
+        var hostBuilder = Host.CreateDefaultBuilder(args)
+            .UseSignalFController()
+            .UseSignalFConfiguration()
+            .ConfigureServices(services =>
+            {
+                    services.AddSignalFControllerService();
+                    services.AddTransient<ISystemConfiguration, MyConfiguration>();
+            });
+
+        var host = hostBuilder.Build();
+
+        await host.RunAsync();
+    }
+}
---
+++ CpuTemperature/Tutorial/TaskExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Scotec.Math.Units;
+using SignalF.Configuration;
+using SignalF.Datamodel.Configuration;
+
+namespace Tutorial;
+
+public static class TaskExtensions
+{
+    private static readonly string OneSecondWriteTask = "1s write task";
+
+    public static ICoreConfiguration AddTasks(this ICoreConfiguration configuration)
+    {
+        configuration.AddTasks(builder =>
+        {
+            builder.AddTask(OneSecondWriteTask, 0, ETaskType.Write, 1.0, Time.Units.Second);
+        });
+
+        return configuration;
+    }
+
+    public static ICoreConfiguration AddTaskMappings(this ICoreConfiguration configuration)
+    {
+        configuration.AddTaskMappings(builder =>
+        {
+            builder.MapSignalProcessorToTask("DefaultProcedure", OneSecondWriteTask);
+        });
+
+        return configuration;
+    }
+}
